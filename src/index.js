6704d6c28c389bdfbe25f31c5d53b9777de8fbd9
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-03-01 14:57:19 +0100

    feat: build es and cjs variants using cli-app-scripts (#8)

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import i18next from 'i18next'
+
+i18next.init({
+    resources: undefined,
+
+    lng: 'en',
+    fallbackLng: 'en',
+
+    debug: false,
+    ns: ['translation'],
+    defaultNS: 'translation',
+    fallbackNS: false,
+    nsSeparator: false,
+
+    returnEmptyString: false,
+    returnObjects: false,
+
+    keySeparator: false,
+    react: {
+        wait: true,
+    },
+})
+
+export default i18next
